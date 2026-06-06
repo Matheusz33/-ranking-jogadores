9fcca86fb76d293717eae2ad2021986428d8d773
Matheusz33 <matheushenriquejk777@gmail.com>
2026-06-06 01:59:33 -0400

    atualiza credenciais MySQL

--- src/RankingServlet.java
+++ src/RankingServlet.java
@@ -10,7 +10,7 @@ public class RankingServlet extends HttpServlet {
 
 private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/ferrovia";
 private static final String USER = "root";
-private static final String PASS = "GfRgaVLRPNzrkTPydkDSXSCGxsysLEDy";
+private static final String PASS = "fLSKxlTTrvkbydKlNkNUhORjOBbAcyRb";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
